5fa2d92bd46ae55cdc31bdfc72e925d20cfecb73
yanglinwei <1962264482@qq.com>
2019-12-11 15:19:35 +0800

    自动补偿以及幂等性问题的解决

--- RabbitMQ-Consumer/src/main/java/com/ylw/springboot/rabbitmq/consumer/FanoutSmsConsumer.java
+++ RabbitMQ-Consumer/src/main/java/com/ylw/springboot/rabbitmq/consumer/FanoutSmsConsumer.java
@@ -1,15 +1,38 @@
 package com.ylw.springboot.rabbitmq.consumer;
 
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
-@RabbitListener(queues = "fanout_sms_queue")
+
 public class FanoutSmsConsumer {
 
+    @RabbitListener(queues = "fanout_sms_queue")
+    public void process(Message message) throws Exception {
+        String messageId = message.getMessageProperties().getMessageId();
+        System.out
+                .println(Thread.currentThread().getName() + ",邮件消费者获取生产者消息msg:" + new String(message.getBody(), "UTF-8")
+                        + ",messageId:" + messageId);
+        /*if(hasFinished(messageId)){
+            //如果消息处理完了，则直接完成，不消费信息
+            return;
+        }*/
+    }
+
+    /*
     @RabbitHandler
     public void process(String msg) {
         System.out.println("短信消费者获取生产者消息msg:" + msg);
+       *//* Result result = OkHttpCientUtils(url,json);
+
+        if(result == null){
+            throw new Exception("调用第三方接口失败");
+        }
+        System.out.println("调用第三方接口与成功");*//*
     }
+    */
+
+
 }
--- RabbitMQ-Producer/src/main/java/com/ylw/springboot/rabbitmq/producer/FanoutProducer.java
+++ RabbitMQ-Producer/src/main/java/com/ylw/springboot/rabbitmq/producer/FanoutProducer.java
@@ -1,19 +1,31 @@
 package com.ylw.springboot.rabbitmq.producer;
 
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.UUID;
 
 @Component
 public class FanoutProducer {
     @Autowired
     private AmqpTemplate amqpTemplate;
 
-    public void send(String queueName) {
+    /*public void send(String queueName) {
         String msg = "my_fanout_msg:" + new Date();
         System.out.println(msg + ":" + msg);
         amqpTemplate.convertAndSend(queueName, msg);
+    }*/
+
+    public void send(String queueName) {
+        String msg = "my_fanout_msg:" + System.currentTimeMillis();
+        Message message = MessageBuilder.withBody(msg.getBytes()).setContentType(MessageProperties.CONTENT_TYPE_JSON)
+                .setContentEncoding("utf-8").setMessageId(UUID.randomUUID() + "").build();
+        System.out.println(msg + ":" + msg);
+        amqpTemplate.convertAndSend(queueName, message);
     }
 }
